f1a5866d45519d77262016d1df105b0249ba59de
Bruno Chauvet <bruno.chauvet@live.fr>
2013-11-26 23:26:15 +1100

    Updated JMeter script

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
-SilentAuction::Application.config.session_store :cookie_store, key: '_silent-auction_session', :expire_after => 24.hours
+SilentAuction::Application.config.session_store :cookie_store, {
+	:key =>           '_ufe_session_id',
+  :path =>          '/',
+  :domain =>        nil,
+  :expire_after =>  nil,
+  :secure =>        false,
+  :httponly =>      true,
+  :cookie_only =>   true
+}
