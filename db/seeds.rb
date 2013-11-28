61a3a624572f22ac4516bc97834897aa1eda7e95
Bruno Chauvet <bruno.chauvet@live.fr>
2013-11-28 21:57:34 +1100

    Added staff names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 ### Configuration
-Preference.create(name: 'END_TIME', value: '2013-11-30 22:30:00+1100')
+Preference.create(name: 'END_TIME', value: '2013-11-30 22:10:00+1100')
 
 ### Data
 ImportUsers.import 'public/gala_tables.xlsx'
@@ -14,4 +14,13 @@ User.create(user_group: staff_table, title: 'Mr', first_name: 'Bruno', last_name
 User.create(user_group: staff_table, title: 'Mrs', first_name: 'Cecile', last_name: 'Reyes-Chauvet', pin: 1234)
 User.create(user_group: staff_table, title: 'Mr', first_name: 'Alexis', last_name: 'Bouvot', pin: 1234)
 User.create(user_group: staff_table, title: 'Mr', first_name: 'Nicolas', last_name: 'Teulier', pin: 1234)
-User.create(user_group: staff_table, title: 'Mr', first_name: 'Denis', last_name: 'Gittar', pin: 1234)
+User.create(user_group: staff_table, title: 'Mr', first_name: 'Denis', last_name: 'Gittard', pin: 1234)
+User.create(user_group: staff_table, title: 'Mr', first_name: 'Claude', last_name: 'Annonier', pin: 1234)
+User.create(user_group: staff_table, title: 'Mrs', first_name: 'Sophie', last_name: 'Jarre', pin: 1234)
+User.create(user_group: staff_table, title: 'Mr', first_name: 'Jeremy', last_name: "O'Connor", pin: 1234)
+User.create(user_group: staff_table, title: 'Mrs', first_name: 'Agathe', last_name: 'Calandreau', pin: 1234)
+User.create(user_group: staff_table, title: 'Mrs', first_name: 'Alyzee', last_name: 'Wyart', pin: 1234)
+User.create(user_group: staff_table, title: 'Mrs', first_name: 'Clemence', last_name: 'Lefranc', pin: 1234)
+User.create(user_group: staff_table, title: 'Mr', first_name: 'Christophe', last_name: 'Blanchys', pin: 1234)
+User.create(user_group: staff_table, title: 'Mr', first_name: 'Nicolas', last_name: 'Chaudhari', pin: 1234)
+User.create(user_group: staff_table, title: 'Mrs', first_name: 'Chantelle', last_name: 'Buultjens', pin: 1234)
